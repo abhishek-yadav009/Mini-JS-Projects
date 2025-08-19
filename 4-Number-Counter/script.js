fd1a2e26d481f71fdc275492d6219f7fa5a4c538
Abhishek Yadav <abhishekyadav66353@gmail.com>
2025-08-19 12:26:52 +0545

    add number counter game

---
+++ 4-Number-Counter/script.js
@@ -0,0 +1,22 @@
+const finalCounter = document.querySelector("#counter")
+const decrease = document.querySelector("#decrease")
+const reset = document.querySelector("#reset")
+const increase = document.querySelector("#increase")
+
+let counter =0
+
+increase.addEventListener("click",() => {
+    counter++
+    finalCounter.innerText = counter
+
+})
+
+decrease.addEventListener("click",()=>{
+    counter--
+    finalCounter.innerText = counter
+})
+
+reset.addEventListener("click",()=>{
+    counter = 0
+    finalCounter.innerText = counter
+})
