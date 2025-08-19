f7595935a58b450438ad661946dcb9fccb1e6e75
Abhishek Yadav <abhishekyadav66353@gmail.com>
2025-08-19 17:55:24 +0545

    add html,css,and js file of quiz app with html,css code but still need to add some code in js file to finish this code

---
+++ 8-Quiz-App-JS/main.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Element", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+
+    },
+    {
+        question: "Which is smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+
+    }
+]
+
+const questionElement = document.querySelector("#question")
+const answerButton = document.querySelector("#answer-buttons")
+const nextButton = document.querySelector("#next-btn")
+const btn = document.querySelectorAll(".btn")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
