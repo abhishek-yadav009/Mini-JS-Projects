081b80c4dde3a9db3206688f4dab3ce127a88cab
Abhishek Yadav <abhishekyadav66353@gmail.com>
2025-08-19 18:42:26 +0545

    add a function on js file to show the wuestion and answers in our page

--- 8-Quiz-App-JS/main.js
+++ 8-Quiz-App-JS/main.js
@@ -26,14 +26,25 @@ const answerButton = document.querySelector("#answer-buttons")
 const nextButton = document.querySelector("#next-btn")
 const btn = document.querySelectorAll(".btn")
 
+// at first we need to init the index
+let currQuestionIndex = 0
+
+// here we create a function and pass the perimeter  for currquestions
+function showQuestion(index) {
+    const currQuestion = questions[index]
+    questionElement.innerText = currQuestion.question
+
+
+    // this is for showing all the answers in their box
+    btn.forEach((btn, i) => {
+        const currAnswer = currQuestion.answers[i].text
+        btn.innerText = currAnswer
+    })
+}
 
 
 
-
-
-
-
-
+showQuestion(currQuestionIndex)
 
 
 
